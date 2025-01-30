5a9c4b23e06ec501b3de09aa2ad8eb0cc6ba9446
roberohn <38044693+jrsimp@users.noreply.github.com>
2025-01-30 20:23:16 +0000

    Renamed title
    Changed from Roberohn to IMJR.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
 
-export const title = 'Roberohn';
+export const title = 'IMJR';
 export const description = 'SvelteKit blog for me';
 export const url = dev ? 'http://localhost:5173/' : 'https://imjr.me/';
