b1e2bd0fa9fc3115b39e0e9645bc38b1887ccf94
roberohn <roberohn@icloud.com>
2025-03-14 10:37:22 +0000

    Forgot to load updated svelte.config.js file with `sql` language added.

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,31 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-import { mdsvex, escapeSvelte } from 'mdsvex'
+import { mdsvex, escapeSvelte } from 'mdsvex';
 import { createHighlighter } from 'shiki';
 import remarkToc from 'remark-toc';
 import rehypeSlug from 'rehype-slug';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-  extensions: ['.md'],
-  layout: {
-    _: './src/mdsvex.svelte'
-  },
-  highlight: {
-    highlighter: async (code, lang = 'text') => {
-      const highlighter = await createHighlighter({
-        themes: ['poimandres'],
-        langs: ['javascript', 'typescript']
-      })
-      await highlighter.loadLanguage('javascript', 'typescript')
-      const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
-      return `{@html \`${ html }\` }`
-    }
-  },
-  remarkPlugins: [[remarkToc, { tight: true}]],
-  rehypePlugins: [rehypeSlug]
-}
+	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript', 'sql']
+			});
+			await highlighter.loadLanguage('javascript', 'typescript', 'sql');
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }));
+			return `{@html \`${html}\` }`;
+		}
+	},
+	remarkPlugins: [[remarkToc, { tight: true }]],
+	rehypePlugins: [rehypeSlug]
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
